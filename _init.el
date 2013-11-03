8989acf24bc3aa00d9a533548dbccfd888c1a1f9
bamanzi <bamanzi@localhost>
2013-11-03 20:17:53 +0800

    configure ibuffer-vc

--- _init.el
+++ _init.el
@@ -208,6 +208,29 @@
 (autoload 'hide-non-matching-lines "hide-lines"
   "Hide lines that don't match the specified regexp." t)
 
+;;** ibuffer-vc
+
+(eval-after-load "ibuffer-vc"
+  `(progn     
+     (add-hook 'ibuffer-hook
+               (lambda ()
+                 (ibuffer-vc-set-filter-groups-by-vc-root)
+                 (unless (eq ibuffer-sorting-mode 'alphabetic)
+                   (ibuffer-do-sort-by-alphabetic))))
+
+     (setq ibuffer-formats
+           '((mark modified read-only vc-status-mini " "
+                   (name 18 18 :left :elide)
+                   " "
+                   (size 9 -1 :right)
+                   " "
+                   (mode 16 16 :left :elide)
+                   " "
+                   (vc-status 16 16 :left)
+                   " "
+                   filename-and-process)))     
+     ))
+
 
 ;;** misc
 (idle-require 'volatile-highlights)
