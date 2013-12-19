72f2e6789a124e0c79ad0405aad9dd0ec3bc87cc
bamanzi <bamanzi@localhost>
2013-12-19 21:01:44 +0800

    move some deprecated configuration to another file

--- _init.el
+++ _init.el
@@ -35,32 +35,6 @@
 
 ;;** vi emulation (viper)
 
-;; ;;*** viper 
-;; (setq viper-expert-level '3)
-;; (setq viper-inhibit-startup-message 't)
-
-;; (defun viper-cua-region-fix()
-;;   (define-key viper-vi-global-user-map [backspace] 'backward-delete-char-untabify)
-;;   (define-key viper-vi-global-user-map "\C-d" 'delete-char)
-;;   (define-key viper-insert-global-user-map [backspace] 'backward-delete-char-untabify)
-;;   (define-key viper-insert-global-user-map "\C-d" 'delete-char))
-
-;; (eval-after-load "viper" `(viper-cua-region-fix))
-
-;; ;;*** vimpulse
-;; (eval-after-load "viper"
-;;   '(require 'vimpulse))
-
-;; ;;*** Ex commands without entering viper-mode
-;; ;; stoem from http://www.advogato.org/person/chalst/diary/277.html
-;; ;;for ex commands supported by viper, refer `ex-token-alist'
-
-;; (require 'viper-ex)
-;; (require 'viper-keym)
-;; (require 'viper-cmd)
-
-;; (define-key global-map (kbd "ESC ESC :") 'viper-ex)
-
 ;;*** viper/vimpulse addons
 (eval-after-load "vimpulse"
   `(progn
@@ -74,69 +48,9 @@
   "Toggle global God mode." t)
 
 
-;;** scratch-ext
 
-(require 'scratch-ext nil t)
-
-(setq inhibit-startup-screen nil)
-;;(setq initial-major-mode 'org-mode)
-(run-with-timer 4 nil 'scratch-load/bmz)
-
-(defun scratch-load/bmz ()
-   "Load today's scratch content."
-   (interactive)
-   (let* ((scratch-ext-log-name-format "%Y/%m/%d.org")
-          (buffer (get-buffer-create "*scratch*"))
-          (file (expand-file-name (format-time-string scratch-ext-log-name-format (current-time))
-                                   scratch-ext-log-directory)))
-     (with-current-buffer buffer
-       (if (file-readable-p file)
-           (progn
-             (erase-buffer)
-             (insert-file-contents file)
-             (funcall initial-major-mode))
-         (message "Scratch log not found. %s" file)
-         (end-of-buffer)
-         (insert-string "(progn\n")
-         (insert-string "  (local-set-key (kbd \"C-x C-s\") 'scratch-save/bmz) ;; save to daily log\n")
-         (insert-string "  (local-set-key (kbd \"C-x C-v\") 'scratch-load/bmz) ;; load from daily log\n")
-         (insert-string "  (local-set-key (kbd \"C-c i\")   'scratch-ext-insert-newest-log) ;; load last log\n")
-         (insert-string "  )\n\n")
-         )
-       (local-set-key (kbd "C-x C-s") 'scratch-save/bmz)
-       (local-set-key (kbd "C-x C-v") 'scratch-load/bmz)    
-       (local-set-key (kbd "C-c i")   'scratch-ext-insert-newest-log))))
-
-(defun scratch-save/bmz ()
-  "Save scratch to daily log."
-  (interactive)
-  (require 'scratch-ext)
-  ;;by default, save by timestamp
-  (let ((scratch-ext-log-directory "~/.scratch.verbose/")
-        (scratch-ext-log-name-format "%Y/%m/%d-%H%M%S"))
-    (scratch-ext-save-scratch-to-file))
-  
-  ;;manually save to daily log
-  (let ((scratch-ext-log-name-format "%Y/%m/%d.org"))
-    (scratch-ext-save-scratch-to-file)))
-
-
-;;*** set org-mode as major-mode of *scratch*
-(if t
-  (eval-after-load "scratch-ext"
-    `(progn
-       ;;redefine this function to add my own keybindings
-       (defun scratch-ext-initialize-buffer-as-scratch (buffer)
-         (with-current-buffer buffer
-           (funcall 'org-mode) ;;hack: use `org-mode' as major mode
-           (local-set-key (kbd "C-x C-s") 'scratch-save) ;;hack: bind C-x C-s to `scratch-save'
-           (local-set-key (kbd "C-c i")   'scratch-ext-insert-newest-log) ;;hack
-           (erase-buffer)
-           (if (and initial-scratch-message
-                    (not inhibit-startup-screen))
-               (insert initial-scratch-message))))
-       ))
-  )
+;;** log scratch contents
+(idle-require 'scratch-log)
 
 
 ;;** tempbuf: kill unused buffers in the background
@@ -169,6 +83,16 @@
 (autoload 'outline-move-subtree-down  "outline-magic"
   "Move the currrent subtree down past ARG headlines of the same level." t)
 
+(eval-after-load "outline-cycle"
+  `(progn
+     (define-key outline-mode-prefix-map (kbd "TAB") 'outline-cycle)
+     ))
+
+;;*** outline-org-like
+(autoload 'outline-org-mode  "outline-org-like"
+  "A special `outline-minor-mode' that use org-mode-style headings." t)
+(autoload 'outline-org-heading-mode "outline-org-like"
+  "eldoc" t)
 
 ;;*** other folding
 (autoload 'hide-region-hide  "hide-region"
@@ -178,6 +102,7 @@
 
 
 ;;** rectangle
+
 (autoload 'rectplus-copy-rectangle  "rect+"
   "Copy rectangle area." t)
 (autoload 'rectplus-insert-number-rectangle  "rect+"
@@ -187,7 +112,7 @@
 (define-key ctl-x-r-map (kbd "M-n") 'rectplus-insert-number-rectangle)
 
 
-;;**  recent-jump
+;;** recent-jump
 (setq rj-column-threshold 100)
 (if (load "recent-jump" t)
     (recent-jump-mode t)
@@ -195,6 +120,7 @@
 
 (global-set-key (kbd "C-c <") 'recent-jump-backward)
 (global-set-key (kbd "C-c >") 'recent-jump-forward)
+
 
 ;;** smart-mode-line
 (idle-require 'smart-mode-line)
@@ -246,6 +172,12 @@
      ))
 
 
+;;** vlf
+(autoload 'vlf "vlf"  "View Large FILE." t)
+
+(idle-require 'vlf)
+
+
 ;;** misc
 (idle-require 'volatile-highlights)
 
@@ -263,5 +195,4 @@
 (idle-require 'info+)
 
 (idle-require 'mouse3)
-
 
