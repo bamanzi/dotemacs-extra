c20a4c5af8b70cb8f4b23acfc5560964623e2694
bamanzi <bamanzi@localhost>
2014-03-11 21:33:19 +0800

    config backup-each-save

--- _init-useful.el
+++ _init-useful.el
@@ -1,188 +1,104 @@
-;; ** yasnippet
-(autoload 'anything-yasnippet-2  "anything-yasnippet-2"
-  "Find yasnippets." t)
+;; ** backup-each-save (and keeping folder structure)
+(idle-require 'backup-each-save)
 
-(global-set-key (kbd "<f5> s")  'anything-yasnippet-2)
-
-;; ** scite-api
-(eval-after-load "auto-complete-config"
-  `(if (load "auto-complete-scite-api" t)
-       (add-to-list 'ac-sources 'ac-source-scite-api)
-     (message "%s: failed to load `auto-complete-scite-api'." load-file-name)))
-
-
-
-;; ** folding
-;; *** outline
-(autoload 'outline-cycle  "outline-magic"
-  "Visibility cycling for outline(-minor)-mode." t)
-(autoload 'outline-move-subtree-up  "outline-magic"
-  "Move the currrent subtree up past ARG headlines of the same level." t)
-(autoload 'outline-move-subtree-down  "outline-magic"
-  "Move the currrent subtree down past ARG headlines of the same level." t)
-
-(eval-after-load "outline-cycle"
+(eval-after-load "backup-each-save"
   `(progn
-     (define-key outline-mode-prefix-map (kbd "TAB") 'outline-cycle)
+     (add-hook 'after-save-hook 'backup-each-save)
+
+     (when (memq system-type '(windows-nt ms-dos cygwin))
+
+         (defun backup-each-save ()
+           (let ((bfn (buffer-file-name)))
+             (when (and (file-exists-p bfn)
+                        (or backup-each-save-remote-files
+                            (not (file-remote-p bfn)))
+                        (funcall backup-each-save-filter-function bfn)
+                        (or (not backup-each-save-size-limit)
+                            (<= (buffer-size) backup-each-save-size-limit)))
+               (copy-file bfn (backup-each-save-compute-location bfn) t t t))))
+
+
+         ;; for windows, remove ':' in backup filename 
+         (defun backup-each-save-compute-location (filename)
+           ;;(let* ((containing-dir (file-name-directory filename))
+           (let* ((containing-dir (replace-regexp-in-string ":" "" (file-name-directory filename)))
+                  (basename (file-name-nondirectory filename))
+                  (backup-container
+                   (format "%s/%s"
+                           backup-each-save-mirror-location
+                           containing-dir)))
+             (when (not (file-exists-p backup-container))
+               (make-directory backup-container t))
+             (format "%s/%s-%s" backup-container basename
+                     (format-time-string backup-each-save-time-format))))
+       )
+     ))
+
+;; ** vi emulation (viper)
+
+;; *** viper/vimpulse addons
+(eval-after-load "vimpulse"
+  `(progn
+     (require 'vimpulse-cjk nil t)
+     (require 'vimpulse-textobj-between nil t)
+     (require 'vimpulse-surround nil t)
      ))
 
 
-;; *** other folding
-(autoload 'hide-region-hide  "hide-region"
-  "Hides a region by making an invisible overlay over it and save the" t)
-(autoload 'hide-region-unhide  "hide-region"
-  "Unhide a region at a time, starting with the last one hidden and" t)
+
+;; ** log scratch contents
+(idle-require 'scratch-log)
 
 
-;; ** rectangle
-
-(autoload 'rectplus-copy-rectangle  "rect+"
-  "Copy rectangle area." t)
-(autoload 'rectplus-insert-number-rectangle  "rect+"
-  "Insert incremental number into each left edges of rectangle's line." t)
-
-(define-key ctl-x-r-map (kbd "M-w") 'rectplus-copy-rectangle)
-(define-key ctl-x-r-map (kbd "M-n") 'rectplus-insert-number-rectangle)
-
-
-;; ** back-button: Visual navigation through mark rings
-;;https://github.com/rolandwalker/back-button
-(if (and (< emacs-major-version 24)
-         (locate-library "smartrep"))
-    (message "WARNING: smartrep isn't compatible with emacs < 23 yet. you should remove it: %s"
-             (locate-library "smartrep"))
-  (idle-require 'back-button))
-
-(eval-after-load "back-button"
+;; ** tempbuf: kill unused buffers in the background
+(idle-require 'tempbuf)
+(eval-after-load "tempbuf"
   `(progn
-     (back-button-mode 1)
-     (define-key goto-map (kbd "<left>")    'back-button-local-backward)
-     (define-key goto-map (kbd "<right>")   'back-button-local-backward)
-     (define-key goto-map (kbd "<M-left>")  'back-button-global-backward)
-     (define-key goto-map (kbd "<M-right>") 'back-button-global-backward)
-
+     (add-hook 'dired-mode-hook 'turn-on-tempbuf-mode)
+     (add-hook 'custom-mode-hook 'turn-on-tempbuf-mode)
+     (if (require 'man nil t)
+         (add-hook 'Man-mode-hook 'turn-on-tempbuf-mode))
+     (add-hook 'view-mode-hook 'turn-on-tempbuf-mode)
+     (add-hook 'diff-mode-hook 'turn-on-tempbuf-mode)
+     (add-hook 'completion-list-mode 'turn-on-tempbf-mode)
+     (add-hook 'occur-mode-hoo 'turn-on-tempbuf-mode)
+     
+     (if (require 'anything nil t)
+         (add-hook 'anything-mode-hook 'turn-on-tempbuf-mode))
+     
+     ;;(add-hook 'w3-mode-hook 'turn-on-tempbuf-mode)
+     
      ))
 
-;; *** recent-jump
-;;(idle-require 'recent-jump)
+;; ** outshine = outline + org-mode
+;;TAB key for org-mode like folding
 
-(autoload 'recent-jump-mode "recent-jump"
-  "Toggle recent-jump mode." t)
-
-(setq rj-column-threshold 100)
-(eval-after-load "recent-jump"
+(eval-after-load "outshine"
   `(progn
-     (define-key goto-map (kbd "<left>")  'recent-jump-backward)
-     (define-key goto-map (kbd "<right>") 'recent-jump-forward)
+     ;;(add-hook 'outline-minor-mode-hook 'outshine-hook-function)
+
+     (set-face-attribute 'outshine-level-1 nil :inherit 'org-level-1)
+     (set-face-attribute 'outshine-level-2 nil :inherit 'org-level-2)
+     (set-face-attribute 'outshine-level-3 nil :inherit 'org-level-3)
+     (set-face-attribute 'outshine-level-4 nil :inherit 'org-level-4)
+     (set-face-attribute 'outshine-level-5 nil :inherit 'org-level-5)
+     (set-face-attribute 'outshine-level-6 nil :inherit 'org-level-6)     
      ))
 
+;; outorg is like "reverse Org-Babel":
+;; call `outorg-edit-as-org' (C-z ') to edit outline(outshine) section in `org-mode',
+;; and `outorg-copy-edits-and-exit' (M-#) to convert back.
 
-;; ** smart-mode-line
-(if (>= emacs-major-version 24)
-    (idle-require 'smart-mode-line)
-  )
+(autoload 'outorg-edit-as-org "outorg"
+  "Convert and copy to temporary Org buffer" t)
 
-(autoload 'sml/setup "smart-mode-line"
-  "Setup the mode-line, or revert it." t)
-
-(eval-after-load "smart-mode-line"
+(eval-after-load "outorg"
   `(progn
-     (sml/setup)
-     (setq sml/hidden-modes '(" RJ"
-                              " drag"
-                              " back"
-                              " org-link"
-                              " Hi"
-                              " -Chg"
-                              " Undo-Tree"))
-
-     (setq-default mode-line-format
-                   (cons " "
-                         (cons '(which-func-mode which-func-format)
-                                (default-value 'mode-line-format))))
+     (load-library "outshine")
      ))
 
-
-;; ** hide some lines
-(autoload 'hide-matching-lines "hide-lines"
-  "Hide lines matching the specified regexp." t)
-(autoload 'hide-non-matching-lines "hide-lines"
-  "Hide lines that don't match the specified regexp." t)
-
-
-;; ** ibuffer-vc
-
-(eval-after-load "ibuffer-vc"
-  `(progn     
-     (add-hook 'ibuffer-hook
-               (lambda ()
-                 (ibuffer-vc-set-filter-groups-by-vc-root)
-                 (unless (eq ibuffer-sorting-mode 'alphabetic)
-                   (ibuffer-do-sort-by-alphabetic))))
-
-     (setq ibuffer-formats
-           '((mark modified read-only vc-status-mini " "
-                   (name 18 18 :left :elide)
-                   " "
-                   (size 9 -1 :right)
-                   " "
-                   (mode 16 16 :left :elide)
-                   " "
-                   (vc-status 16 16 :left)
-                   " "
-                   filename-and-process)))     
-     ))
-
-
-;; ** vlf
-(autoload 'vlf "vlf"  "View Large FILE." t)
-
-(idle-require 'vlf)
-
-
-;; ** desktop-registry
-(autoload 'desktop-registry-change-desktop  "desktop-registry"
-  "Change to the desktop named NAME." t)
-
-(global-set-key (kbd "<f12> <f12>") 'desktop-registry-change-desktop)
-
-(idle-require 'desktop-registry)
-
-(eval-after-load "desktop-registry"
-  `(progn
-     (desktop-registry-auto-register 1)
-     ))
-
-(unless (fboundp 'file-name-base)
-  (defun file-name-base (&optional filename)
-    "Return the base name of the FILENAME: no directory, no extension.
-FILENAME defaults to `buffer-file-name'."
-    (file-name-sans-extension
-     (file-name-nondirectory (or filename (buffer-file-name))))))
-
-(require 'cl-lib)
-(unless (fboundp 'cl-find)
-  (defalias 'cl-find 'find))
-
-
-;; ** misc
-(autoload 'yagist-list "yagist"
-  "Displays a list of all of the current user's gists in a new buffer." t)
-
-(idle-require 'volatile-highlights)
-
-(idle-require 'dired+)
-
-(idle-require 'buff-menu+)
-(eval-after-load "buff-menu+"
-  `(progn
-     (load-library "buff-menu")))
-
-;;(idle-require 'vc+) ;;disabled as it's buggy
-
-(idle-require 'menu-bar+)
-
-(idle-require 'info+)
-
-(idle-require 'mouse3)
-
+;; *** outline-org-like (my old package similar to `outshine')
+(autoload 'outline-org-mode  "outline-org-like"
+  "A special `outline-minor-mode' that use org-mode-style headings." t)
+(autoload 'outline-org-heading-mode "outline-org-like"
+  "eldoc" t)
