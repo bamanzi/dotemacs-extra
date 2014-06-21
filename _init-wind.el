52409252cc3160c747c683fc2d871624ad86bd64
bamanzi <bamanzi@localhost>
2014-06-21 23:08:14 +0800

    add some keybindings for window manuplating

--- _init-wind.el
+++ _init-wind.el
@@ -1,8 +1,10 @@
-;; ** windows
+;; * windows
 (idle-require 'bmz-window-misc)
 
 (eval-after-load "bmz-window-misc"
   `(progn
+     (global-set-key (kbd "<f11> g")     'ido-jump-to-window)
+     
      (global-set-key (kbd "<f11> <M-left>")  'move-buffer-left)
      (global-set-key (kbd "<f11> <M-right>") 'move-buffer-right)
      (global-set-key (kbd "<f11> <M-up>")    'move-buffer-up)
@@ -16,6 +18,19 @@
      (global-set-key (kbd "<f11> <M-backspace>") 'rotate-windows)
      (global-set-key (kbd "<f11> |")    'window-toggle-split-direction)
      
+     ))
+
+(idle-require 'window-extension)
+
+(eval-after-load "window-extension"
+  `(progn
+     (global-set-key (kbd "<f11> <f11>") 'toggle-one-window)
+     (global-set-key (kbd "<f11> 1 v")   'delete-other-windows-vertically+)
+     (global-set-key (kbd "<f11> 1 h")   'delete-other-windows-horizontally+)
+     
+     (global-set-key [(control x) (?0)] 'sticky-window-delete-window)
+     (global-set-key [(control x) (?1)] 'sticky-window-delete-other-windows)
+     (global-set-key [(control x) (?9)] 'sticky-window-keep-window-visible)
      ))
 
 
@@ -45,3 +60,8 @@
 ;; *** frame-bufs
 (autoload 'frame-bufs-mode "frame-bufs"
   "Toggle frame-bufs-mode on and off." t)
+
+
+
+
+
