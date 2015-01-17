2ff81c6350983b5c05fe6a68861bbc7faaec5d9a
bamanzi <bamanzi@localhost>
2015-01-17 22:59:46 +0800

    refactoring symbol-jump: make it extensible

--- _init-prog.el
+++ _init-prog.el
@@ -13,52 +13,80 @@
 
 
 ;;** super+click to jump to declaration/implementation
-(defun symbol-jump-on-mouse-down (event)
+(defvar symbol-jump/function-alist
+  '((emacs-lisp-mode       . symbol-jump/elisp-jump-to-symbol)
+    (lisp-interaction-mode . symbol-jump/elisp-jump-to-symbol)
+    (help-mode             . symbol-jump/elisp-jump-to-symbol)
+    (c-mode                . semantic-complete-jump)
+    (java-mode             . semantic-complete-jump)
+    (python-mode           . semantic-complete-jump)
+    (ruby-mode             . robe-jump))
+  "functions used in `symbol-jump' for each major mode.
+
+Each function should accept ONE argument in STRING type.")
+
+(idle-require 'pulse)
+
+(defun symbol-jump ()
+  (interactive)
+  (let* ( (symbol (thing-at-point 'symbol))
+          (symbol-text (progn
+                         (set-text-properties 0 (length symbol) nil symbol)
+                         symbol))
+          (jump-method (assoc major-mode symbol-jump/function-alist)) )
+    (if jump-method
+        (progn
+          (message "call `%s' to jump to `%s'" jump-method symbol-text)
+          (apply jump-method (cdr symbol-text))
+          (when (and pulse-command-advice-flag (interactive-p))
+            (pulse-momentary-highlight-one-line (point))))
+      (message "no configuration for `%s' in `symbol-jump/function-alist'." major-mode))))
+
+;; *** jump functions
+(defun symbol-jump/imenu-goto-symbol (foo)
+  (interactive )
+  (imenu (thing-at-point 'symbol)))
+
+(defun symbol-jump/elisp-jump-to-symbol (symbol-text)
+  (interactive (list ((read-string "Symbol: "
+                                   (thing-at-point 'symbol)))))
+  (require 'bmz-elisp-misc)
+  (find-symbol-at-point (intern-soft symbol-text)))
+
+(defun symbol-jump/elisp-describe-symbol (symbol-text)
+  (require 'bmz-elisp-misc)
+  (describe-symbol-at-point  (intern-soft symbol-text)))
+
+;; *** mouse
+(defun symbol-jump/on-mouse-down (event)
   (interactive "e")
   (mouse-set-point event)
   (let* ( (begin  (save-excursion (re-search-backward "\\_<") (point)))
           (end    (save-excursion (re-search-forward  "\\_>") (point)))
           (symbol (buffer-substring begin end))
           (ov     (make-overlay begin end)) )
-    (overlay-put ov 'category "linkify-imenu")
+    (overlay-put ov 'category "symbol-jump")
     (overlay-put ov 'face '(:underline t)    )
   ))
 
-(defun symbol-jump-remove-overlay (point)
+(defun symbol-jump/remove-overlay (point)
   (let ( (overlays (overlays-at (point))) )
     (mapcar '(lambda (ov)
-               (if (string= (overlay-get ov 'category) "linkify-imenu")
+               (if (string= (overlay-get ov 'category) "symbol-jump")
                    (delete-overlay ov)))
             overlays
             )
          ))
 
-(defun symbol-jump-on-mouse-up-imenu ()
-  (interactive )
-  (symbol-jump-remove-overlay (point))
-  (imenu (thing-at-point 'symbol))
-  (when (and pulse-command-advice-flag (interactive-p))
-    (pulse-momentary-highlight-one-line (point)))
-  )
+(defun symbol-jump/on-mouse (event)
+  (interactive "e")
+  (mouse-set-point event)
+  (symbol-jump/remove-overlay (point))
+  (call-interactively 'symbol-jump))
+  
 
-(defun symbol-jump-on-mouse-up-among-files ()
-  (interactive )
-  (symbol-jump-remove-overlay (point))
-  (let ( (symbol (thing-at-point 'symbol)) )
-    ;; (message "go to symbol: %s" symbol)
-    (cond
-     ( (memq major-mode '(emacs-lisp-mode lisp-interaction-mode help-mode))
-       (if (fboundp (intern symbol))
-           (find-function-at-point)
-         (find-variable-at-point)))
-     ( (and 'semantic-mode
-            (memq major-mode '(c-mode java-mode python-mode)))
-       (call-interactively 'semantic-complete-jump) )
-     (t
-      (call-interactively 'find-tag)))))
+(define-key global-map (kbd "<s-down-mouse-1>") 'symbol-jump/on-mouse-down)
+(define-key global-map (kbd "<s-mouse-1>")      'symbol-jump/on-mouse)
 
-(define-key global-map (kbd "<s-down-mouse-1>") 'symbol-jump-on-mouse-down)
-(define-key global-map (kbd "<s-mouse-1>")      'symbol-jump-on-mouse-up-imenu)
-(define-key global-map (kbd "<C-s-mouse-1>")    'symbol-jump-on-mouse-up-among-files)
 (define-key global-map (kbd "<s-mouse-3>")      'pop-to-mark-command)
 (define-key global-map (kbd "<C-s-mouse-3>")    'pop-global-mark)
