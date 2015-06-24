7934d1fae2002f21292c5d3f7dcc866b66d462d8
Ba Manzi <bamanzi@gmail.com>
2015-06-24 13:50:14 +0800

    add & configure package git-gutter

--- _init-vc.el
+++ _init-vc.el
@@ -22,6 +22,26 @@
                    filename-and-process)))     
      ))
 
+;; ** git-gutter
+;; https://github.com/syohex/emacs-git-gutter
+(autoload 'git-gutter:toggle "git-gutter"
+  "toggle to show diff information" t)
+
+(defun _frame-reinit-git-gutter (&optional frame)
+  "Choose `git-gutter' implementation from `git-gutter.el' or `git-gutter-fringe.el'."
+  (interactive)
+  (if (require 'git-gutter-fringe nil t)
+      (if (display-graphic-p)
+          (setq git-gutter:init-function 'git-gutter-fr:init
+                git-gutter:view-diff-function 'git-gutter-fr:view-diff-infos
+                git-gutter:clear-function 'git-gutter-fr:clear)
+        (setq git-gutter:init-function nil
+                git-gutter:view-diff-function 'git-gutter:view-diff-infos
+                git-gutter:clear-function 'git-gutter:clear-diff-infos))))
+
+;;(add-hook 'after-make-frame-functions '_frame-reinit-git-gutter)
+
+
 ;; ** git-gutter+
 ;; |                          | git-gutter+.el | git-gutter-fringe+.el |
 ;; | Works in tty frame       | +              | -                     |
