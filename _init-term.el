9728e32cf2e2e0af9f04cbbd054d911e2885c8b2
Ba Manzi <bamanzi@gmail.com>
2016-02-29 19:53:34 +0800

    add configuration for emamux

--- _init-term.el
+++ _init-term.el
@@ -9,3 +9,28 @@
   (anything  '(anything-c-source-lacarte)))
 
 (define-key global-map (kbd "<f5> <f10>") 'anything-lacarte)
+
+;; ** tmux integration
+(autoload 'emamux:send-command "emamux"
+  "Send command to target-session of tmux" t)
+(autoload 'emamux:run-command "emamux"
+  "Run command" t)
+(autoload 'emamux:send-region "emamux"
+  "Send region to target-session of tmux" t) ;; emamux > 0.13 required
+
+(setq emamux:completing-read-type 'ido)
+
+(defun emamux:chdir-pwd-other-pane ()
+  "Send 'cd `pwd`' to other pane."
+  (interactive)
+  (require 'emamux nil 'noerror)
+  (emamux:check-tmux-running)
+  (if (< (string-to-number (shell-command-to-string "tmux list-panes -t: | wc -l")) 2)
+      (emamux:split-runner-pane))
+  (let ((target ":.+") ;; the other pane
+        (cmd (concat "cd " default-directory)))
+    (emamux:reset-prompt target)
+    (emamux:send-keys cmd target)
+    ))
+ 
+(define-key global-map (kbd "<f12> t") 'emamux:chdir-pwd-other-pane)
