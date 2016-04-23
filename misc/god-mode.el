2ea52ff7cfb8747a06c7257035d4ddcafb146484
Ba Manzi <bamanzi@gmail.com>
2016-04-23 17:21:31 +0800

    update god-mode to latest version (20151016)

--- misc/god-mode.el
+++ misc/god-mode.el
@@ -3,10 +3,12 @@
 ;; Copyright (C) 2013 Chris Done
 ;; Copyright (C) 2013 Magnar Sveen
 ;; Copyright (C) 2013 Rüdiger Sonderfeld
+;; Copyright (C) 2013 Dillon Kearns
+;; Copyright (C) 2013 Fabián Ezequiel Gallina
 
 ;; Author: Chris Done <chrisdone@gmail.com>
 ;; URL: https://github.com/chrisdone/god-mode
-;; Version: 2.7.3
+;; Version: 2.15.0
 
 ;; This file is not part of GNU Emacs.
 
@@ -27,44 +29,24 @@
 
 ;;; Commentary:
 
-;; This library defines the following mapping:
-;;
-;; 1. All commands are assumed to be C-<something> unless otherwise
-;;    indicated. Examples:
-;;    * a    -> C-a
-;;    * s    -> C-s
-;;    * akny -> C-a C-k C-n C-y
-;;   * `x s`  → `C-x s`
-;;
-;;   Note the use of space to produce `C-x s`.
-;;
-;; 2. `g' is a special key to indicate M-<something>. This means that
-;;    there is no way to write `C-g' in this mode, you must therefore
-;;    type `C-g' directly. Examples:
-;;    * gf -> M-f
-;;    * gx -> M-f
-;;
-;;    This key can be configured.
-;;
-;; 6. There is a convention of uppercase special keys to indicate
-;;    two modifier keys in action. Those are:
-;;    * Gx -> C-M-x
-;;
-;; 7. There is a literal interpretation key as `l' that can
-;;    be used on chained commands, e.g.
-;;
-;;    * xs -> C-x C-s
-;;    * xlb -> C-x b
-;;    * xlh -> C-x h
-;;    * xp -> C-x C-p
-;;    * xx -> C-x C-x
-;;
-;;    This key can be configured.
-;;
-;; 8. There is a key (default `i') to disable God mode, similar to
-;;    Vim's i.
+;; See README.md.
 
 ;;; Code:
+
+(require 'cl-lib)
+
+(add-hook 'after-change-major-mode-hook 'god-mode-maybe-activate)
+
+(defvar god-local-mode-paused nil)
+(make-variable-buffer-local 'god-local-mode-paused)
+
+(defcustom god-mod-alist
+  '((nil . "C-")
+    ("g" . "M-")
+    ("G" . "C-M-"))
+  "List of keys and their associated modifer."
+  :group 'god
+  :type '(alist))
 
 (defcustom god-literal-key
   " "
@@ -74,26 +56,69 @@
 
 (defcustom god-exempt-major-modes
   '(dired-mode
-    help-mode
     grep-mode
-    magit-log-edit-mode
-    magit-status-mode
     vc-annotate-mode
-    package-menu-mode
-    Buffer-menu-mode
-    git-rebase-mode)
+    git-commit-mode  ; For versions prior to Magit 2.1.0
+    magit-popup-mode)
   "List of major modes that should not start in god-local-mode."
   :group 'god
   :type '(function))
 
+(defcustom god-exempt-predicates
+  (list #'god-exempt-mode-p
+        #'god-comint-mode-p
+        #'god-git-commit-mode-p
+        #'god-view-mode-p
+        #'god-special-mode-p)
+  "List of predicates checked before enabling god-local-mode.
+All predicates must return nil for god-local-mode to start."
+  :group 'god
+  :type '(repeat function))
+
+(defvar god-local-mode-map
+  (let ((map (make-sparse-keymap)))
+    (suppress-keymap map t)
+    (define-key map [remap self-insert-command] 'god-mode-self-insert)
+    (let ((i ?\s))
+      (while (< i 256)
+        (define-key map (vector i) 'god-mode-self-insert)
+        (setq i (1+ i)))
+      (define-key map (kbd "DEL") nil))
+    map))
+
+(defvar god-mode-universal-argument-map
+  (let ((map (copy-keymap universal-argument-map)))
+    (define-key map (kbd "u") 'universal-argument-more)
+    map)
+  "Keymap used while processing \\[universal-argument] with god-mode on.")
+
+;;;###autoload
+(define-minor-mode god-local-mode
+  "Minor mode for running commands."
+  nil " God" god-local-mode-map
+  (if god-local-mode
+      (run-hooks 'god-mode-enabled-hook)
+    (run-hooks 'god-mode-disabled-hook)))
+
+(defun god-local-mode-pause ()
+  "Pause god-mode local to the buffer, if it's
+enabled. See also `god-local-mode-resume'."
+  (when god-local-mode
+    (god-local-mode -1)
+    (setq god-local-mode-paused t)))
+
+(defun god-local-mode-resume ()
+  "Will re-enable god-mode, if it was active when
+`god-local-mode-pause' was called. If not, nothing happens."
+  (when (bound-and-true-p god-local-mode-paused)
+    (setq god-local-mode-paused nil)
+    (god-local-mode 1)))
+
 (defvar god-global-mode nil
   "Activate God mode on all buffers?")
 
-(defvar god-mode-last-command nil
-  "Last command ran. Used for repetition.")
-
-(defvar god-mode-last-prefix-arg nil
-  "Last previous arg, for repetition.")
+(defvar god-literal-sequence nil
+  "Activated after space is pressed in a command sequence.")
 
 ;;;###autoload
 (defun god-mode ()
@@ -104,113 +129,175 @@
       (god-local-mode 1)
     (god-local-mode -1)))
 
-(defvar god-local-mode-map
-  (let ((map (make-sparse-keymap)))
-    (suppress-keymap map t)
-    (define-key map [remap self-insert-command] 'god-mode-self-insert)
-    (define-key map (kbd "g") 'god-mode-meta)
-    (define-key map (kbd "G") 'god-mode-control-meta)
-    (define-key map (kbd "z") 'god-mode-repeat)
-    (define-key map (kbd "i") 'god-local-mode)
-    map))
-
 ;;;###autoload
-(define-minor-mode god-local-mode
-  "Minor mode for running commands."
-  nil " God" god-local-mode-map
-  (if god-local-mode
-      (run-hooks 'god-mode-enabled-hook)
-    (run-hooks 'god-mode-disabled-hook)))
-
-(defun god-mode-meta ()
-  "The command for M-."
+(defun god-mode-all ()
+  "Toggle God mode in all buffers."
   (interactive)
-  (god-mode-try-command "M-" "M-%s"))
+  (let ((new-status (if (bound-and-true-p god-local-mode) -1 1)))
+    (setq god-global-mode t)
+    (mapc (lambda (buffer)
+            (with-current-buffer buffer
+              (god-mode-activate new-status)))
+          (buffer-list))
+    (setq god-global-mode (= new-status 1))))
 
-(defun god-mode-control-meta ()
-  "The command for C-M-."
-  (interactive)
-  (god-mode-try-command "C-M-" "C-M-%s"))
+(defadvice save&set-overriding-map
+  (before god-mode-add-to-universal-argument-map (map) activate compile)
+  "This is used to set special keybindings after C-u is
+pressed. When god-mode is active, intercept the call to add in
+our own keybindings."
+  (if (and god-local-mode (equal universal-argument-map map))
+      (setq map god-mode-universal-argument-map)))
 
 (defun god-mode-self-insert ()
   "Handle self-insert keys."
   (interactive)
-  (let ((key (char-to-string (aref (this-command-keys-vector) 0))))
-    (god-mode-interpret-key key)))
+  (let* ((initial-key (aref (this-command-keys-vector)
+                            (- (length (this-command-keys-vector)) 1)))
+         (binding (god-mode-lookup-key-sequence initial-key)))
+    (when (god-mode-upper-p initial-key)
+      (setq this-command-keys-shift-translated t))
+    (setq this-original-command binding)
+    (setq this-command binding)
+    ;; `real-this-command' is used by emacs to populate
+    ;; `last-repeatable-command', which is used by `repeat'.
+    (setq real-this-command binding)
+    (setq god-literal-sequence nil)
+    (if (commandp binding t)
+        (call-interactively binding)
+      (execute-kbd-macro binding))))
 
-(defun god-mode-repeat ()
-  "Repeat the last command."
+(defun god-mode-upper-p (char)
+  "Is the given char upper case?"
+  (and (>= char ?A)
+       (<= char ?Z)
+       (/= char ?G)))
+
+(defun god-mode-lookup-key-sequence (&optional key key-string-so-far)
+  "Lookup the command for the given `key' (or the next keypress,
+if `key' is nil). This function sometimes
+recurses. `key-string-so-far' should be nil for the first call in
+the sequence."
   (interactive)
-  (when god-mode-last-command
-    (let ((current-prefix-arg god-mode-last-prefix-arg))
-      (call-interactively god-mode-last-command))))
+  (let ((sanitized-key
+         (if key-string-so-far (char-to-string (or key (read-event key-string-so-far)))
+           (god-mode-sanitized-key-string (or key (read-event key-string-so-far))))))
+    (god-mode-lookup-command
+     (key-string-after-consuming-key sanitized-key key-string-so-far))))
 
-(defun god-mode-interpret-key (key)
-  "Interpret the given key. This function sometimes recurses."
-  (cond
-   ;; Digit argument
-   ((string-match "^[0-9]$" key)
-    (let ((current-prefix-arg
-           (if (numberp current-prefix-arg)
-               (string-to-number (concat (number-to-string current-prefix-arg)
-                                         key))
-             (string-to-number key))))
-      (god-mode-interpret-key (char-to-string (read-event (format "%d" current-prefix-arg))))))
-   ;; Boolean prefix arguments
-   ((string= key "u")
-    (let ((current-prefix-arg t))
-      (god-mode-interpret-key (char-to-string (read-event "u")))))
-   ;; For better keyboard macro interpretation.
-   ((string= key " ") (god-mode-interpret-key "SPC"))
-   ;; By default all other things are C-*///
-   (t
-    (let* ((formatted (format "C-%s" key))
-           (command (read-kbd-macro formatted))
-           (binding (key-binding command)))
-      (god-mode-execute-binding formatted binding)))))
+(defun god-mode-sanitized-key-string (key)
+  "Convert any special events to textual."
+  (cl-case key
+    (tab "TAB")
+    (?\  "SPC")
+    (left "<left>")
+    (right "<right>")
+    (prior "<prior>")
+    (next "<next>")
+    (backspace "DEL")
+    (return "RET")
+    (t (char-to-string key))))
 
-(defun god-mode-try-command (prompt format &optional keymapp control)
-  "Try to run a command that takes additional key presses."
-  (let* ((event (read-event prompt))
-         (key. (if (eq event 'backspace)
-                   "DEL"
-                 (char-to-string event))))
-    (let* ((control (if (string= key. god-literal-key) nil control))
-           (key (if (string= key. god-literal-key)
-                    (char-to-string (read-event prompt))
-                  key.))
-           (formatted (format (if keymapp
-                                  (if control
-                                      (concat format " C-%s")
-                                    (concat format " %s"))
-                                format)
-                              key))
-           (command (read-kbd-macro formatted))
-           (binding (key-binding command)))
-      (god-mode-execute-binding formatted binding))))
+(defun key-string-after-consuming-key (key key-string-so-far)
+  "Interpret god-mode special keys for key (consumes more keys if
+appropriate). Append to keysequence."
+  (let ((key-consumed t) next-modifier next-key)
+    (message key-string-so-far)
+    (setq next-modifier
+          (cond
+           ((string= key god-literal-key)
+            (setq god-literal-sequence t)
+            "")
+           (god-literal-sequence
+            (setq key-consumed nil)
+            "")
+           ((and
+             (stringp key)
+             (not (eq nil (assoc key god-mod-alist)))
+             (not (eq nil key)))
+            (cdr (assoc key god-mod-alist)))
+           (t
+            (setq key-consumed nil)
+            (cdr (assoc nil god-mod-alist))
+            )))
+    (setq next-key
+          (if key-consumed
+              (god-mode-sanitized-key-string (read-event key-string-so-far))
+            key))
+    (if key-string-so-far
+        (concat key-string-so-far " " next-modifier next-key)
+      (concat next-modifier next-key))))
 
-(defun god-mode-execute-binding (formatted binding)
+(defun god-mode-lookup-command (key-string)
   "Execute extended keymaps such as C-c, or if it is a command,
 call it."
-  (cond ((commandp binding)
-         (setq this-original-command binding)
-         (setq this-command binding)
-         (setq god-mode-last-command binding)
-         (setq god-mode-last-prefix-arg current-prefix-arg)
-         (call-interactively binding))
-        ((keymapp binding)
-         (god-mode-try-command formatted formatted t t))
-        (:else
-         (error "God: Unknown key binding for `%s`" formatted))))
+  (let* ((key-vector (read-kbd-macro key-string t))
+         (binding (key-binding key-vector)))
+    (cond ((commandp binding)
+           (setq last-command-event (aref key-vector (- (length key-vector) 1)))
+           binding)
+          ((keymapp binding)
+           (god-mode-lookup-key-sequence nil key-string))
+          (:else
+           (error "God: Unknown key binding for `%s`" key-string)))))
 
-(add-hook 'after-change-major-mode-hook 'god-mode-maybe-activate)
+;;;###autoload
+(defun god-mode-maybe-activate (&optional status)
+  "Activate God mode locally on individual buffers when appropriate."
+  (when (not (minibufferp))
+    (god-mode-activate status)))
 
-(defun god-mode-maybe-activate ()
+(defun god-mode-activate (&optional status)
   "Activate God mode locally on individual buffers when appropriate."
   (when (and god-global-mode
-             (not (minibufferp))
-             (not (memq major-mode god-exempt-major-modes)))
-    (god-local-mode 1)))
+             (god-passes-predicates-p))
+    (god-local-mode (if status status 1))))
+
+(defun god-exempt-mode-p ()
+  "Return non-nil if major-mode is exempt.
+Members of the `god-exempt-major-modes' list are exempt."
+  (memq major-mode god-exempt-major-modes))
+
+(defun god-mode-child-of-p (major-mode parent-mode)
+  "Return non-nil if MAJOR-MODE is derived from PARENT-MODE."
+  (let ((parent (get major-mode 'derived-mode-parent)))
+    (cond ((eq parent parent-mode))
+          ((not (null parent))
+           (god-mode-child-of-p parent parent-mode))
+          (t nil))))
+
+(defun god-comint-mode-p ()
+  "Return non-nil if major-mode is child of comint-mode."
+  (god-mode-child-of-p major-mode 'comint-mode))
+
+(defun god-special-mode-p ()
+  "Return non-nil if major-mode is child of special-mode."
+  (god-mode-child-of-p major-mode 'special-mode))
+
+(defun god-view-mode-p ()
+  "Return non-nil if view-mode is enabled in current buffer."
+  view-mode)
+
+(defun god-git-commit-mode-p ()
+  "Return non-nil if a `git-commit-mode' will be enabled in this buffer."
+  (and (bound-and-true-p global-git-commit-mode)
+       ;; `git-commit-filename-regexp' defined in the same library as
+       ;; `global-git-commit-mode'.  Expression above maybe evaluated
+       ;; to true because of autoload cookie.  So we perform
+       ;; additional check.
+       (boundp 'git-commit-filename-regexp)
+       buffer-file-name
+       (string-match-p git-commit-filename-regexp buffer-file-name)))
+
+(defun god-passes-predicates-p ()
+  "Return non-nil if all `god-exempt-predicates' return nil."
+  (not
+   (catch 'disable
+     (let ((preds god-exempt-predicates))
+       (while preds
+         (when (funcall (car preds))
+           (throw 'disable t))
+         (setq preds (cdr preds)))))))
 
 (provide 'god-mode)
 
